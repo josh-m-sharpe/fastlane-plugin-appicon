a01764d6228c65e19f9f9afb693a00532fa30b7d
Josh Sharpe <josh.m.sharpe@gmail.com>
2019-09-07 21:43:03 -0500

    handle foreground and round android icons

--- lib/fastlane/plugin/appicon/actions/android_appicon_action.rb
+++ lib/fastlane/plugin/appicon/actions/android_appicon_action.rb
@@ -51,6 +51,13 @@ module Fastlane
 
           image.resize "#{width}x#{height}"
           image.write basepath + filename
+
+          if icon['device'] == 'launcher'
+            foreground_size = ((width * 2.25) - width) / 2
+            # https://github.com/dlemstra/Magick.NET/issues/57
+            system "convert #{basepath + filename} -bordercolor none -border #{foreground_size}x#{foreground_size} #{basepath}/#{params[:appicon_filename]}_foreground.png"
+            system "convert #{fname} -alpha set \\( +clone -distort DePolar 0 -virtual-pixel HorizontalTile -background None -distort Polar 0 \\) -compose Dst_In -composite -trim +repage -resize #{width}x#{height} #{basepath}/#{params[:appicon_filename]}_round.png"
+          end
         end
 
         UI.success("Successfully stored launcher icons at '#{params[:appicon_path]}'")
